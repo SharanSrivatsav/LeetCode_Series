5aaa558c1ffd3cf719cb1bf8ae1c67eb4a42caec
SharanSrivatsav <166700340+SharanSrivatsav@users.noreply.github.com>
2025-06-25 15:39:05 +0530

    Time: 0 ms (100%), Space: 41.4 MB (55.58%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.java
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -2,34 +2,32 @@ class Solution {
     public int splitArray(int[] nums, int k) {
         int st = 0;
         int en = 0;
-        for(int i : nums){
-            st=Math.max(i,st);
-            en+=i;
+        for (int i : nums) {
+            st = Math.max(i, st);
+            en += i;
         }
-        while(st<en){
-            int m = st+(en-st)/2;
-            if(isPos(nums,k,m)){
-                en=m;
-            }
-            else{
-                st=m+1;
+        while (st < en) {
+            int m = st + (en - st) / 2;
+            if (isPos(nums, k, m)) {
+                en = m;
+            } else {
+                st = m + 1;
             }
         }
         return st;
-        
+
     }
-    public static boolean isPos(int [] nums,int k , int v){
+    public static boolean isPos(int[] nums, int k, int v) {
         int c = 1;
-        int s =0;
-        for(int i : nums){
-            if(s+i>v){
+        int s = 0;
+        for (int i : nums) {
+            if (s + i > v) {
                 c++;
-                s=i;
-            }
-            else{
-                s+=i;
+                s = i;
+            } else {
+                s += i;
             }
         }
-        return c<=k;
+        return c <= k;
     }
 }
