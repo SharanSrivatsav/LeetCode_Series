f21d01a440102957e1e9c1b51c7a10ea6f2c736b
SharanSrivatsav <166700340+SharanSrivatsav@users.noreply.github.com>
2025-06-25 14:58:19 +0530

    Time: 24 ms (91.25%), Space: 63.6 MB (7.01%) - LeetHub

--- 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.java
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.java
@@ -1,29 +1,28 @@
 class Solution {
     public int maximumCandies(int[] candies, long k) {
-        // Arrays.sort(candies);
         int st = 1;
         int en = 0;
-        for(int i :candies){
-            en=Math.max(en,i);
+        for (int i : candies) {
+            en = Math.max(en, i);
         }
-        int ans =0;
-        while(st<=en){
-            int m = (st+en)/2;
-            if(isVal(m,candies,k)){
-                ans=m;
-                st=m+1;
-            }
-            else{
-                en=m-1;
+        int ans = 0;
+        while (st <= en) {
+            int m = (st + en) / 2;
+            if (isVal(m, candies, k)) {
+                ans = m;
+                st = m + 1;
+            } else {
+                en = m - 1;
             }
         }
         return ans;
     }
-    public static boolean isVal(int div,int nums [], long k){
+
+    public static boolean isVal(int div, int nums[], long k) {
         long c = 0;
-        for(int i=0;i<nums.length;i++){
-            c+=(nums[i])/div;
+        for (int i = 0; i < nums.length; i++) {
+            c += (nums[i]) / div;
         }
-        return c>=k;
+        return c >= k;
     }
 }
