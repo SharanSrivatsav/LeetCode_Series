0816595648f3c2b9dbc3332a2d49237b32d18446
SharanSrivatsav <166700340+SharanSrivatsav@users.noreply.github.com>
2025-06-29 11:32:25 +0530

    Time: 2 ms (98.61%), Space: 43.7 MB (89.5%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -5,6 +5,7 @@ class Solution {
         int l =0;
         for(int i=0;i<s.length();i++){
             char c = s.charAt(i);
+            // sliding window when condition fails we drastically reduce the window size()
             if(ind[c]!=-1&&ind[c]>=l){
                 l= ind[c]+1;
             }
