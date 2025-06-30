bb3603238e245f985946893aac4dace43b4adbcb
SharanSrivatsav <166700340+SharanSrivatsav@users.noreply.github.com>
2025-06-30 16:39:40 +0530

    Time: 1 ms (99.3%), Space: 41.1 MB (95.77%) - LeetHub

--- 1010-powerful-integers/1010-powerful-integers.java
+++ 1010-powerful-integers/1010-powerful-integers.java
@@ -4,6 +4,9 @@ class Solution {
             return new ArrayList<>();
         }
         List<Integer> list = new ArrayList<>();
+        // for 1st power we are adding all possible powers of y value that is bound
+        // if we take x^0 how much can we accomodate from y^0,y^1,.....y^n 
+        // such that the sum always less than or equal to "bound"
         for(int i = 1;i<bound;i=i*x){
             for(int j=1;j+i<=bound;j=j*y){
                 if(!list.contains(i+j)){
