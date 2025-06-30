54732910a7dc5fb2e714cc84277819254001fcc9
SharanSrivatsav <166700340+SharanSrivatsav@users.noreply.github.com>
2025-06-30 16:53:30 +0530

    Time: 0 ms (100%), Space: 42.7 MB (81.72%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        solve(nums,temp,list,0);
+        return list;
+    }
+    public static void solve(int nums[],List<Integer> temp ,List<List<Integer>> list, int st ){
+        list.add(new ArrayList<>(temp));
+        if(st==nums.length){
+            return;
+        }
+        for(int i=st;i<nums.length;i++){
+            temp.add(nums[i]);
+            solve(nums,temp,list,i+1);
+            temp.remove(temp.size()-1);
+        }
+    }
+}
