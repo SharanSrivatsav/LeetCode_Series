7efb5dad2d618df4ce5e0e8fd866fbaa1e1722e2
SharanSrivatsav <166700340+SharanSrivatsav@users.noreply.github.com>
2025-06-30 16:35:42 +0530

    Time: 1 ms (99.3%), Space: 41.1 MB (95.77%) - LeetHub

---
+++ 1010-powerful-integers/1010-powerful-integers.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<Integer> powerfulIntegers(int x, int y, int bound) {
+        if(bound<2){
+            return new ArrayList<>();
+        }
+        List<Integer> list = new ArrayList<>();
+        for(int i = 1;i<bound;i=i*x){
+            for(int j=1;j+i<=bound;j=j*y){
+                if(!list.contains(i+j)){
+                    list.add(i+j);
+                }
+                if(y==1) break;
+            }
+            if(x==1) break;
+        }
+        return list;
+    }
+}
