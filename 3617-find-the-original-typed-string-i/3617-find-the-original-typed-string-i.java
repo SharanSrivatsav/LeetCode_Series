58e9bd73b6c36f804df9b3881445809ee6fde78c
SharanSrivatsav <166700340+SharanSrivatsav@users.noreply.github.com>
2025-07-01 10:32:39 +0530

    Time: 0 ms (100%), Space: 42.2 MB (61.16%) - LeetHub

--- 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.java
+++ 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.java
@@ -1,6 +1,7 @@
 class Solution {
     public int possibleStringCount(String word) {
-        int c = 1; 
+        int c = 1; // we are keeping the c itself as 1
+        // because there is chance that given string migth be the original one
         for(int i = word.length()-1;i>=1;i--){
              if(word.charAt(i)==word.charAt(i-1)){
                 c++;
