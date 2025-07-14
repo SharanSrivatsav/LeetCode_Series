c95317b7ac0b6f8b077f742edd5580aa20d84fed
SharanSrivatsav <166700340+SharanSrivatsav@users.noreply.github.com>
2025-07-14 11:50:40 +0530

    Time: 3 ms (96.62%), Space: 44.6 MB (17.79%) - LeetHub

---
+++ 0437-path-sum-iii/0437-path-sum-iii.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int count = 0;
+    public int pathSum(TreeNode root, int targetSum) {
+        HashMap<Long,Integer> map = new HashMap<>();
+        count=0;
+        map.put((long)0,1);
+        solver(root,targetSum,0,map);
+        return count;
+    }
+    public void solver(TreeNode node, int tar , long sum, HashMap<Long,Integer> map){
+        if(node==null){
+            return;
+        }
+        sum+=node.val;
+        if(map.containsKey(sum-tar)){
+            count+=map.get(sum-tar);
+        }
+        map.put(sum,map.getOrDefault(sum,0)+1);
+        solver(node.left,tar,sum,map);
+        solver(node.right,tar,sum,map);
+        map.put(sum,map.get(sum)-1);
+        if(map.get(sum)==0){
+            map.remove(sum);
+        }
+    }
+}
