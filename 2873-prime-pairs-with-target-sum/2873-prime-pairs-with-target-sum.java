57bc80ce64229a7f328da46d8a549adc21d8cb9c
SharanSrivatsav <166700340+SharanSrivatsav@users.noreply.github.com>
2025-08-28 15:32:14 +0530

    Time: 184 ms (41.98%), Space: 53.3 MB (74.06%) - LeetHub

---
+++ 2873-prime-pairs-with-target-sum/2873-prime-pairs-with-target-sum.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<Integer>> findPrimePairs(int n) {
+        List<List<Integer>> res =  new ArrayList<>();
+        if(n<=3){
+            return res;
+        }
+        boolean sieve [] = new boolean [n+1];sieve[1] = true;sieve[0] =true;
+        for(int i=2;i<=n;i++){
+            if(!sieve[i]){
+                for(long j=(long)i*i;j<=n;j+=i){
+                    sieve[(int)j]=true;
+                }
+            }
+        }
+        for (int i = 2; i <= n / 2; i++) {
+            int comp = n - i;
+            if (!sieve[i] && !sieve[comp]) {
+                res.add(Arrays.asList(i, comp));
+            }
+        }
+        return res;
+    }
+}
