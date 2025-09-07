7597a0f265495f4fe25784c05d9131bc77f935bc
SharanSrivatsav <166700340+SharanSrivatsav@users.noreply.github.com>
2025-09-07 21:20:50 +0530

    Time: 27 ms (75.6%), Space: 60.5 MB (25.25%) - LeetHub

---
+++ 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int minimumSize(int[] nums, int maxOperations) {
+        /*if isPos(){
+            reduce the search space
+        }*/
+        int max = -1;
+        for(int i: nums){
+           max=Math.max(i,max);
+        }
+        int st=1;
+        int en=max;
+        int ans= -1;
+        while(st<=en){
+            int m = st+(en-st)/2;
+            if(isPos(nums,m,maxOperations)){
+               ans=m;
+               en=m-1;
+            }
+            else{
+               st=m+1;
+            }
+        }
+        return ans;
+    }
+    public boolean isPos(int nums[],int val, int maxCount){
+        int c= 0;
+        for(int i :nums){
+            if(i>val){
+                c+=(i-1)/val;
+                if(c>maxCount){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
